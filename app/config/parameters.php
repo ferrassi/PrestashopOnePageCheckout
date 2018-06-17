960bea6f960960093f0dbd98435021c16af00f1b
Badr FERRASSI <badr.ferrassi@gmail.com>
2018-06-17 16:18:13 +0200

    readme.md

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '2yrBiKpxvDt0UzfBEy6szqAWkUnfAVCoGgErbsOO3DHyT9PomgxKcLqP',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-06-02',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'aimSrXz0QzMaXZM54sUoUZJq1UyC4ABTBnp36w6bCAPpvDTYT2Ahn259',
+    'cookie_iv' => '4ar6tEAI',
+    'new_cookie_key' => 'def00000fb290e418e4d6555f4a6eafe96b1cd30f3c999abc1eb8e9cf338fabb9362b5784a1acad34642cd1c88be7ef90ed70da17bf9242a25a2fe74a8afe79b321566fb',
+  ),
+);
